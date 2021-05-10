db16fbee5fe0d1cf08f74a9455388f1a337ec2c8
laksh-21 <iamlakshable@gmail.com>
2021-05-10 22:32:05 +0530

    Add Conflict Resolving in DB

--- app/src/main/java/com/example/weatherby/Data/WeatherDBHelper.java
+++ app/src/main/java/com/example/weatherby/Data/WeatherDBHelper.java
@@ -10,9 +10,11 @@ import com.example.weatherby.Data.WeatherContract.WeatherEntry;
 public class WeatherDBHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "weather.db";
-    private static final int DATABASE_VERSION = 1;
 
-    public WeatherDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+//  !!!INCREMENT THIS EACH TIME YOU UPDATE THE SCHEMA OR OnUpgrade WILL NOT BE CALLED!!!
+    private static final int DATABASE_VERSION = 3;
+
+    public WeatherDBHelper(@Nullable Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
@@ -22,20 +24,22 @@ public class WeatherDBHelper extends SQLiteOpenHelper {
         final String SQL_WEATHER_TABLE_CREATE =
                 "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
                 WeatherEntry._ID                + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                WeatherEntry.COLUMN_WEATHER_ID  + " INTEGER, "  +
-                WeatherEntry.COLUMN_DATE        + " INTEGER, "  +
-                WeatherEntry.COLUMN_MAX_TEMP    + " REAL, "     +
-                WeatherEntry.COLUMN_MIN_TEMP    + " REAL, "     +
-                WeatherEntry.COLUMN_HUMIDITY    + " REAL, "     +
-                WeatherEntry.COLUMN_PRESSURE    + " REAL, "     +
-                WeatherEntry.COLUMN_WIND_SPEED  + " REAL, "     +
-                WeatherEntry.COLUMN_DEGREES     + " REAL"       + " );";
+                WeatherEntry.COLUMN_WEATHER_ID  + " INTEGER NOT NULL, "  +
+                WeatherEntry.COLUMN_DATE        + " INTEGER NOT NULL, "  +
+                WeatherEntry.COLUMN_MAX_TEMP    + " REAL NOT NULL, "     +
+                WeatherEntry.COLUMN_MIN_TEMP    + " REAL NOT NULL, "     +
+                WeatherEntry.COLUMN_HUMIDITY    + " REAL NOT NULL, "     +
+                WeatherEntry.COLUMN_PRESSURE    + " REAL NOT NULL, "     +
+                WeatherEntry.COLUMN_WIND_SPEED  + " REAL NOT NULL, "     +
+                WeatherEntry.COLUMN_DEGREES     + " REAL NOT NULL, "     +
+                "UNIQUE (" + WeatherEntry.COLUMN_DATE + ") ON CONFLICT REPLACE" + ");";
 
         db.execSQL(SQL_WEATHER_TABLE_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL("DROP TABLE IF EXISTS " + WeatherEntry.TABLE_NAME);
+        onCreate(db);
     }
 }
