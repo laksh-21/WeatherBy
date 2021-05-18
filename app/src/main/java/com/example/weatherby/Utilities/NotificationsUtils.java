921b9720fcace30de14375e3fc3b40e179f8b02d
laksh-21 <iamlakshable@gmail.com>
2021-05-18 22:38:49 +0530

    Add NotificationUtils polish

--- app/src/main/java/com/example/weatherby/Utilities/NotificationsUtils.java
+++ app/src/main/java/com/example/weatherby/Utilities/NotificationsUtils.java
@@ -5,8 +5,31 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 public class NotificationsUtils {
     private static final String NOTIFICATION_CHANNEL_ID = "notification_channel_id";
+    private static final int NOTIFICATION_ID = 6199;
+
+    public static void showNotif(Context context){
+        createChannel(context);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                context,
+                NOTIFICATION_CHANNEL_ID)
+                .setContentTitle("This is the title")
+                .setContentText("Here is the text that is in the descripton");
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN && Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
+            builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        }
+
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+    }
 
     private static void createChannel(Context context){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
