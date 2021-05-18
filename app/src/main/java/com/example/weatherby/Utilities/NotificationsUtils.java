f33b67bf864ab804aca9406f67c712b4a9fdcde1
laksh-21 <iamlakshable@gmail.com>
2021-05-19 02:05:29 +0530

    Add NotificationTapAction

--- app/src/main/java/com/example/weatherby/Utilities/NotificationsUtils.java
+++ app/src/main/java/com/example/weatherby/Utilities/NotificationsUtils.java
@@ -2,17 +2,21 @@ package com.example.weatherby.Utilities;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import com.example.weatherby.MainActivity;
 import com.example.weatherby.R;
 
 public class NotificationsUtils {
     private static final String NOTIFICATION_CHANNEL_ID = "notification_channel_id";
     private static final int NOTIFICATION_ID = 6199;
+    private static final int INTENT_CODE = 9411;
 
     public static void showNotification(Context context){
         createChannel(context);
@@ -23,7 +27,11 @@ public class NotificationsUtils {
                 .setContentTitle("This is the title")
                 .setContentText("Here is the text that is in the descripton")
                 // small icon is absolutely necessary
-                .setSmallIcon(R.drawable.ic_launcher_foreground);
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                // it sets the tap action
+                .setContentIntent(getTapAction(context))
+                // it dismisses the notification if user taps one it
+                .setAutoCancel(true);
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN && Build.VERSION.SDK_INT < Build.VERSION_CODES.O){
             builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
@@ -33,6 +41,16 @@ public class NotificationsUtils {
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
         notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    private static PendingIntent getTapAction(Context context){
+        Intent mainActivityIntent = new Intent(context, MainActivity.class);
+
+        return PendingIntent.getActivity(context,
+                INTENT_CODE,
+                mainActivityIntent,
+                0
+                );
     }
 
     private static void createChannel(Context context){
