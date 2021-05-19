4ce629c8c90af4a1a69fe738f107358e992f57a6
laksh-21 <iamlakshable@gmail.com>
2021-05-19 23:19:23 +0530

    Add SyncUtils

---
+++ app/src/main/java/com/example/weatherby/Utilities/WeatherSyncUtils.java
@@ -0,0 +1,11 @@
+package com.example.weatherby.Utilities;
+
+public class WeatherSyncUtils {
+    public final static String NOTIFICATION_CANCEL_ACTION = "notification-cancel-action";
+
+    public static void executeTask(String action){
+        if(action.equals(NOTIFICATION_CANCEL_ACTION)){
+
+        }
+    }
+}
