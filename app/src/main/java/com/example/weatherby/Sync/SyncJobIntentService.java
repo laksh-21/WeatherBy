ff5983c040340682a7762ec2f49016034ee4d3a0
laksh-21 <iamlakshable@gmail.com>
2021-05-19 23:33:46 +0530

    Add SyncUtils

--- app/src/main/java/com/example/weatherby/Sync/SyncJobIntentService.java
+++ app/src/main/java/com/example/weatherby/Sync/SyncJobIntentService.java
@@ -1,4 +1,14 @@
 package com.example.weatherby.Sync;
 
-public class SyncJobIntentService {
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.JobIntentService;
+
+public class SyncJobIntentService extends JobIntentService {
+
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+
+    }
 }
