dc4ebb67cb4740705e164a598ec3309a4b4b57b4
laksh-21 <iamlakshable@gmail.com>
2021-05-20 13:19:47 +0530

    Add SyncWeather

--- app/src/main/java/com/example/weatherby/Sync/WeatherSyncUtils.java
+++ app/src/main/java/com/example/weatherby/Sync/WeatherSyncUtils.java
@@ -2,7 +2,11 @@ package com.example.weatherby.Sync;
 
 import android.content.Context;
 
+import com.example.weatherby.Utilities.NetworkUtils;
 import com.example.weatherby.Utilities.NotificationsUtils;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class WeatherSyncUtils {
     public final static String NOTIFICATION_CANCEL_ACTION = "notification-cancel-action";
@@ -10,6 +14,18 @@ public class WeatherSyncUtils {
     public static void executeTask(Context context, String action){
         if(action.equals(NOTIFICATION_CANCEL_ACTION)){
             NotificationsUtils.cancelNotifications(context);
+        }
+    }
+
+    synchronized public static void syncWeather(Context context){
+        try {
+            URL weatherURL = NetworkUtils.buildWeatherUrl("Kolkata");
+
+            String jsonResponse = NetworkUtils.getHttpResponse(weatherURL);
+
+            // reformat the parse JSON thingy to send out an array of ContentValues
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
