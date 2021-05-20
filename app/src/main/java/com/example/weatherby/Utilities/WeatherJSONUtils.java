c83ecb394bda9a487b929467a42440c59ab555bb
laksh-21 <iamlakshable@gmail.com>
2021-05-20 13:37:56 +0530

    Fix JSON Parsing

--- app/src/main/java/com/example/weatherby/Utilities/WeatherJSONUtils.java
+++ app/src/main/java/com/example/weatherby/Utilities/WeatherJSONUtils.java
@@ -1,6 +1,9 @@
 package com.example.weatherby.Utilities;
 
+import android.content.ContentValues;
 import android.content.Context;
+
+import com.example.weatherby.Data.WeatherContract;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,8 +24,11 @@ public class WeatherJSONUtils {
      final static String TEMP_MAX_CODE = "max";
      final static String TEMP_NIGHT_CODE = "night";
      final static String TEMP_MORNING_CODE = "morn";
+    private static final String WIND_SPEED_CODE = "speed";
+    private static final String WIND_DIRECTION_CODE = "deg";
 
      final static String WEATHER_DESC_CODE = "weather";
+    private static final String WEATHER_ID_CODE = "id";
      final static String WEATHER_MAIN_CODE = "main";
      final static String WEATHER_SUM_CODE = "description";
 
@@ -46,20 +52,44 @@ public class WeatherJSONUtils {
         int count = forecastList.length();
 
         String[] parsedForecastList = new String[count];
+        ContentValues[] contentValues = new ContentValues[count];
 
         for(int pos = 0; pos<count; pos++){
+            long dateTimeInMillis;
+            double pressure;
+            int humidity;
+            double windSpeed;
+            double windDirection;
+            int weatherId;
+
             JSONObject posForecast = forecastList.getJSONObject(pos);
 
-            long epochTime = posForecast.getLong(EPOCH_CODE) * 1000L;
-            String date = WeatherUnitUtils.convertEpochToDate(epochTime);
+            dateTimeInMillis = posForecast.getLong(EPOCH_CODE) * 1000L;
+            pressure = posForecast.getDouble(PRESSURE_CODE);
+            humidity = posForecast.getInt(HUMIDITY_CODE);
+            windSpeed = posForecast.getDouble(WIND_SPEED_CODE);
+            windDirection = posForecast.getDouble(WIND_DIRECTION_CODE);
 
             JSONObject tempObj = posForecast.getJSONObject(TEMP_DESC_CODE);
             double minTemp = tempObj.getDouble(TEMP_MIN_CODE);
             double maxTemp = tempObj.getDouble(TEMP_MAX_CODE);
 
-            String minMaxTemp = WeatherUnitUtils.factorMinMaxTemp(context, minTemp, maxTemp);
+            JSONObject weatherObj = posForecast.getJSONArray(WEATHER_DESC_CODE).getJSONObject(0);
 
-            parsedForecastList[pos] = date + " " + minMaxTemp;
+            weatherId = weatherObj.getInt(WEATHER_ID_CODE);
+
+            ContentValues contentValue = new ContentValues();
+
+            contentValue.put(WeatherContract.WeatherEntry.COLUMN_DATE, dateTimeInMillis);
+            contentValue.put(WeatherContract.WeatherEntry.COLUMN_HUMIDITY, humidity);
+            contentValue.put(WeatherContract.WeatherEntry.COLUMN_PRESSURE, pressure);
+            contentValue.put(WeatherContract.WeatherEntry.COLUMN_WIND_SPEED, windSpeed);
+            contentValue.put(WeatherContract.WeatherEntry.COLUMN_DEGREES, windDirection);
+            contentValue.put(WeatherContract.WeatherEntry.COLUMN_MAX_TEMP, maxTemp);
+            contentValue.put(WeatherContract.WeatherEntry.COLUMN_MIN_TEMP, minTemp);
+            contentValue.put(WeatherContract.WeatherEntry.COLUMN_WEATHER_ID, weatherId);
+
+            contentValues[pos] = contentValue;
         }
         return parsedForecastList;
     }
